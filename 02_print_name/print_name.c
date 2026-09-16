79a10ea3acc62f03569fc1bde658238a93570107
Sahil <sahiledu2007@gmail.com>
2026-09-16 19:26:49 +0600

    03_c_program to take user input and display it, so i took 2 integer and isplayed it

--- 02_print_name/print_name.c
+++ 02_print_name/print_name.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
-    char name[50];
-    scanf("%s", name);
-    printf("Hello %s", name);
+
+    printf("Sahil");
+
     return 0;
 }
---
+++ 03_input_output/input_output.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+
+    int a, b;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("\n\n\nEner the first integer: %d\n", a);
+    printf("Ener the second integer: %d\n\n\n\n\n\ Done", b);
+
+    return 0;
+}
