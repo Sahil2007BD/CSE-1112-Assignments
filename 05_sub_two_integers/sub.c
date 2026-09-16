ff24951363e6fdea6067900bb4d7a8a93ebb0358
Sahil <sahiledu2007@gmail.com>
2026-09-16 19:37:13 +0600

    added c program to substract two integer

---
+++ 05_sub_two_integers/sub.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a, b, sum;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+    sum = a - b;
+    printf("\n\nSubstraction is: %d\n\n\n\n\n", sum);
+    return 0;
+}
