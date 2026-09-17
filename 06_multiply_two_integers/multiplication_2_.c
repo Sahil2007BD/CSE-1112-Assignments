d57368723c1457e50e2d46886623220321641a6d
Sahil <sahiledu2007@gmail.com>
2026-09-17 22:45:33 +0600

    added cprograam to mutlitply 2 integers

---
+++ 06_multiply_two_integers/multiplication_2_.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a, b, sum;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+    sum = a * b;
+    printf("\n\n\nMultiplication is: %d\n\n\n\n\n", sum);
+    return 0;
+    
+}
