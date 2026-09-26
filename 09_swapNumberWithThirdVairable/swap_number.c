143187b87d49a50d841aebac6fc00e9855bfeaec
Mohammed Sahil Hoda <sahiledu2007@gmail.com>
2026-09-26 17:51:57 +0600

    Refactor number swapping to use a temporary variable

--- 09_swapNumberWithThirdVairable/swap_number.c
+++ 09_swapNumberWithThirdVairable/swap_number.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-int main(){
-    int x,y;
-    scanf("%d%d",&x,&y);
-    x=x+y;
-    y=x-y;
-    x=x-y;
-    printf("%d %d",x,y);
+
+int main() {
+    int x, y, temp;
+
+    scanf("%d%d", &x, &y);
+
+    temp = x;
+    x = y;
+    y = temp;
+
+    printf("%d %d", x, y);
+
     return 0;
 }
