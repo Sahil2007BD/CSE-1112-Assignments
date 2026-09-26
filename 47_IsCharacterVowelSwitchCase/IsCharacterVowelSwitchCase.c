cd90f1fa7bf3a3f18acd48285c8c48fb3f8d3f8e
Sahil <sahiledu2007@gmail.com>
2026-09-26 14:00:27 +0600

    Added Programs to assingment

---
+++ 47_IsCharacterVowelSwitchCase/IsCharacterVowelSwitchCase.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    char x;
+    scanf(" %c",&x);
+    switch(x){
+        case 'a': case 'e': case 'i': case 'o': case 'u':
+        case 'A': case 'E': case 'I': case 'O': case 'U':
+            printf("Vowel"); break;
+        default:
+            printf("Not Vowel");
+    }
+    return 0;
+}
